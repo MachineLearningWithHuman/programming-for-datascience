763504ab2aa99b33692f68e844c29b2e3154301b
satyajit_cm <ssatyjitmaitra1@outlook.com>
2019-07-12 15:16:52 +0530

    Add Via Upload

---
+++ statistics/weighted_mean.py
@@ -0,0 +1,7 @@
+#calculate weighted mean without numpy
+
+N = map(int,input().split())
+X = list(map(int, input().strip().split(' ')))
+W = list(map(int, input().strip().split(' ')))
+sum_X = sum([a*b for a,b in zip(X,W)])
+print("weighted mean:",round((sum_X/sum(W)),1)) 
